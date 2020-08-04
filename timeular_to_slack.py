34775598ae190ff4bbd473a760de988ccb3f3219
Dennis Webb <dennis@bluesentryit.com>
2020-08-04 18:04:23 -0500

    adds ability to update slack profile

--- timeular_to_slack.py
+++ timeular_to_slack.py
@@ -1,29 +1,38 @@
+import aws_lambda_logging
+import logging
+import os
+
 from apiclient import APIClient, JsonRequestFormatter, JsonResponseHandler, HeaderAuthentication
-import json, logging, os
+from slack import WebClient
 
 TIMEULAR_ACTIVITY_TO_SLACK_STATUS = {
     'default': {
         'status': '',
-        'emoji': ':smile:'
+        'emoji': ':thumbs_up:',
+        'snooze': False
     },
     'Working': {
         'status': 'Focused Work',
-        'emoji': ':thinking_face:'
+        'emoji': ':thinking_face:',
+        'snooze': True
     },
     'Meeting': {
         'status': 'In a meeting',
-        'emoji': ':calendar:'
+        'emoji': ':calendar:',
+        'snooze': True
     }
 }
 
+
 class TimeularClient(APIClient):
-    _api_key = None
-    _api_secret = None
-    _api_token = None
-    _base_url = None
+    _api_key: str = None
+    _api_secret: str = None
+    _api_token: str = None
+    _base_url: str = None
 
     def __init__(self, base_url='https://api.timeular.com/api/v2', api_key='', api_secret=''):
-        super(TimeularClient, self).__init__(request_formatter=JsonRequestFormatter, response_handler=JsonResponseHandler)
+        super(TimeularClient, self).__init__(request_formatter=JsonRequestFormatter,
+                                             response_handler=JsonResponseHandler)
         self._api_key = api_key
         self._api_secret = api_secret
         self._base_url = base_url
@@ -32,22 +41,30 @@ class TimeularClient(APIClient):
         self.set_authentication_method(authentication_method=HeaderAuthentication(token=self._api_token))
 
     def get_access_token(self):
-        url = f"{self._base_url}/developer/sign-in"
+        url: str = f"{self._base_url}/developer/sign-in"
         result = self.post(url, data={"apiKey": self._api_key, "apiSecret": self._api_secret})
         return result
 
     def get_tracking(self):
-        url = f"{self._base_url}/tracking"
+        url: str = f"{self._base_url}/tracking"
         result = self.get(url)
         return result
 
-def main(event, context):
-    config = {}
-    config['TIMEULAR_API_KEY'] = os.environ.get('TIMEULAR_API_KEY')
-    config['TIMEULAR_API_SECRET'] = os.environ.get('TIMEULAR_API_SECRET')
-    config['SLACK_API_TOKEN'] = os.environ.get('SLACK_API_TOKEN')
 
-    # very simple validation for expected env variables:
+def main(event, context):
+    config = {'DEBUG_LEVEL': os.getenv('DEBUG_LEVEL', 'WARN'),
+              'DEBUG_BOTO_LEVEL': os.getenv('DEBUG_BOTO_LEVEL', 'CRITICAL'),
+              'SLACK_API_TOKEN': os.environ.get('SLACK_API_TOKEN'),
+              'SLACK_SNOOZE_DURATION': os.getenv('SLACK_SNOOZE_DURATION', '60'),
+              'TIMEULAR_API_KEY': os.environ.get('TIMEULAR_API_KEY'),
+              'TIMEULAR_API_SECRET': os.environ.get('TIMEULAR_API_SECRET')
+              }
+
+    aws_lambda_logging.setup(
+        level=config['DEBUG_LEVEL'],
+        boto_level=config['DEBUG_BOTO_LEVEL']
+    )
+
     for c in config:
         if config[c] is None:
             logging.error('Environment variable {} not found.'.format(c))
@@ -55,11 +72,27 @@ def main(event, context):
 
     api = TimeularClient(api_key=config['TIMEULAR_API_KEY'], api_secret=config['TIMEULAR_API_SECRET'])
 
-    current_activity = 'default'
+    current_activity: str = 'default'
 
     current_tracking = api.get_tracking()['currentTracking']
 
     if current_tracking:
         current_activity = current_tracking['activity']['name']
+        logging.debug(f"Timeular tracking activity {current_activity}")
 
+    slack_status = TIMEULAR_ACTIVITY_TO_SLACK_STATUS[current_activity]
+    new_status: str = slack_status['status']
+    new_emoji: str = slack_status['emoji']
 
+    logging.info(f"Setting Slack status to {new_emoji} {new_status}")
+    slack_client: WebClient = WebClient(token=config['SLACK_API_TOKEN'])
+    slack_client.users_profile_set(profile={"status_text": new_status, "status_emoji": new_emoji})
+
+    if slack_status['snooze']:
+        if not slack_client.dnd_info()['snooze_enabled']:
+            logging.info(f"Turning Slack snooze on for {config['SLACK_SNOOZE_DURATION']}")
+            slack_client.dnd_setSnooze(num_minutes=int(config['SLACK_SNOOZE_DURATION']))
+    else:
+        if slack_client.dnd_info()['snooze_enabled']:
+            logging.info(f"Turning Slack snooze off")
+            slack_client.dnd_endSnooze()
